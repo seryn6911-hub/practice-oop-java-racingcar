5ffcaa39a41995d01eb77268a52797b71fcdb94a
seryn6911-hub <seryn6911@gmail.com>
2025-09-21 17:23:29 +0900

    수정

--- src/main/java/racingcar/View.java
+++ src/main/java/racingcar/View.java
@@ -1,6 +1,5 @@
 package racingcar;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,8 +9,7 @@ public class View {
     public View() {
         this.scanner = new Scanner(System.in);
     }
-
-    // 경주차 이름 입력
+    //경주차 이름 입력
     public String[] getMemberNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         List<String> lst = List.of(scanner.nextLine().split(","));
@@ -28,14 +26,11 @@ public class View {
             }
         }
 
-        String[] result = new String[lst.size()];
-        for (int i = 0; i < lst.size(); i++) {
-            result[i] = lst.get(i).trim();
-        }
-        return result;
+        return lst.stream()
+                .map(String::trim)
+                .toArray(String[]::new);
     }
-
-    // 시도 회수 결정
+    //시도 회수 결정
     public int getTryCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         int tryNum = scanner.nextInt();
@@ -44,21 +39,15 @@ public class View {
         }
         return tryNum;
     }
-
-    // 경주차 간거 표시
+    //경주차 간거 표시
     public void printProgress(List<Member> members) {
         for (Member m : members) {
             System.out.println(m.getName() + " : " + "-".repeat(m.getGo()));
         }
         System.out.println();
     }
-
-    // 우승자 발표
+    //우승자 발표
     public void printWinners(List<Member> winners) {
-        List<String> winnerNames = new ArrayList<>();
-        for (Member winner : winners) {
-            winnerNames.add(winner.getName());
-        }
-        System.out.println("최종 우승자 : " + String.join(", ", winnerNames));
+        System.out.println("최종 우승자 : " + String.join(", ", winners.stream().map(Member::getName).toList()));
     }
 }
